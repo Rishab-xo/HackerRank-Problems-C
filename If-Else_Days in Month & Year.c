a6b7208110add88351b76e9a512ccb3400452b67
Rishab <rishabx0@gmail.com>
2025-02-07 22:26:01 +0530

    Add files via upload

---
+++ If-Else_Days in Month & Year.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    
+        int month,year;
+    scanf("%d %d", &month, &year);
+    
+        if(month<=0 || month>12){
+        printf("Invalid Month\n");
+            
+    }
+    
+    if(year<=0 || year>1000000){
+        printf("Invalid Year");
+        return 0;
+    }
+    switch(month){
+            case 1 :printf("31");
+            break;
+              case 2 :
+            if((year%4==0 && year%100!=0) ||  year%400==0){
+              printf("29");  
+            }
+            
+            else{
+                printf("28");
+            }
+            break;
+              case 3 :printf("31");
+            break;
+              case 4 :printf("30");
+            break;
+              case 5 :printf("31");
+            break;
+              case 6 :printf("30");
+            break;
+              case 7 :printf("31");
+            break;
+              case 8 :printf("31");
+            break;
+              case 9 :printf("30");
+            break;
+              case 10 :printf("31");
+            break;
+              case 11:printf("30");
+            break;
+              case 12:printf("31");
+            break;
+    }
+    
+    return 0;
+}
